47c9496467d59a9ca4634f1ded8899aaf245ec58
Victoriia <nosareva.vs@gmail.com>
2022-10-04 11:47:13 +0300

    fixed constructor style v1

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,12 @@
-'use strict'
-let dataUrl = 'https://nosarevavs.ru/php/data.php'
+"use strict";
+let dataUrl = "https://nosarevavs.ru/php/data.php";
 
 async function sendForm(form) {
-    let response = await fetch(dataUrl, {
-        method: 'POST',
-        body: new FormData(form)
-    });
-    if (response.ok) {
-        let result = await response.text();
-    }
+  let response = await fetch(dataUrl, {
+    method: "POST",
+    body: new FormData(form),
+  });
+  if (response.ok) {
+    let result = await response.text();
+  }
 }
